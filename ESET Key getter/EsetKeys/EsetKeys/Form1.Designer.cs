b1e18779de2f0d7425b669241549e21112854485
Rumesh Eranga <rehrumesh@hotmail.com>
2013-09-20 18:56:00 +0530

    Initial Commit with version 1

---
+++ ESET Key getter/EsetKeys/EsetKeys/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace EsetKeys
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.gv = new System.Windows.Forms.DataGridView();
+            this.uname = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pass = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.gv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Get Keys";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // gv
+            // 
+            this.gv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gv.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.uname,
+            this.pass});
+            this.gv.Location = new System.Drawing.Point(12, 54);
+            this.gv.Name = "gv";
+            this.gv.Size = new System.Drawing.Size(295, 195);
+            this.gv.TabIndex = 1;
+            // 
+            // uname
+            // 
+            this.uname.DataPropertyName = "Username";
+            this.uname.HeaderText = "Username";
+            this.uname.MinimumWidth = 100;
+            this.uname.Name = "uname";
+            this.uname.Width = 150;
+            // 
+            // pass
+            // 
+            this.pass.DataPropertyName = "Password";
+            this.pass.HeaderText = "Password";
+            this.pass.Name = "pass";
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(107, 18);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(35, 13);
+            this.lbl1.TabIndex = 2;
+            this.lbl1.Text = "label1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 256);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(198, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Keys are taken from www.eset325.com .";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 271);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(278, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Developer is not responsible for the accuracy of the keys.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(319, 299);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.gv);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Eset Keys";
+            ((System.ComponentModel.ISupportInitialize)(this.gv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView gv;
+        private System.Windows.Forms.DataGridViewTextBoxColumn uname;
+        private System.Windows.Forms.DataGridViewTextBoxColumn pass;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ESET Key getter/EsetKeys/EsetKeys/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EsetKeys
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            lbl1.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                lbl1.Text = "Please wait. Data is fetching from the server";
+                gv.DataSource = null;
+                var source = new BindingSource();
+                KeyList kl = getKeys();
+                source.DataSource = kl.Kies;
+                gv.DataSource = source;
+                gv.Columns[0].Width = 150;
+                lbl1.Text = "";
+            }
+            catch (Exception ex) {
+                MessageBox.Show("Error Occured.Please try again later. And make sure to check your internet connection.");
+                lbl1.Text = "Connection error.";
+            }
+        }
+
+        private KeyList getKeys()
+        {
+            WebClient wc = new WebClient();
+            string httpRawData = wc.DownloadString(new Uri("http://anonymouse.org/cgi-bin/anon-www.cgi/http://www.nod325.com", UriKind.Absolute));
+            string[] words = Regex.Split(httpRawData, "<p>Username:");
+
+            KeyList kl = new KeyList();
+            
+            for (int i = 1; i < words.Length;i++ )
+            {
+                extractKeys(words[i], ref kl);
+            }
+            
+            return kl;
+        }
+
+        private void extractKeys(string s,ref KeyList tmp) {
+            string[] out1 = Regex.Split(s,"<br />");
+            string Username = out1[0];
+            string[] out2 = Regex.Split(out1[1],"</p>");
+            string pass = Regex.Split((Regex.Split(out2[0], "\r\n"))[1], "Password:")[1];
+
+            KeyPair k = new KeyPair();
+            k.Username = Username;
+            k.Password = pass;
+
+            tmp.Kies.Add(k);
+
+        }
+    }
+
+    class KeyPair {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+
+    class KeyList {
+        public List<KeyPair> Kies { get; set; }
+
+        public KeyList() {
+            Kies = new List<KeyPair>();
+        }
+    }
+}
